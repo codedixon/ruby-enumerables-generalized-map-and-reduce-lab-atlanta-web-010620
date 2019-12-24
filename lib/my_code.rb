1c7fbee538296953a2a311a9413f50c529b65423
Rashad Dixon <dixoncodes@gmail.com>
2019-12-24 21:38:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,10 +15,9 @@ def reduce(source_array, starting_point = 0)
   while i < source_array.length
   if source_array[i] == false
     total = false
-    i = source_array.length
+    i += 1
   else
   total = yield(starting_point, source_array[i])
-  i = 1
   end
   i += 1
   end
