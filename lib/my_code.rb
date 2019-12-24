bcde24c044f5af3f4cac588e0676c2b94978fb82
Rashad Dixon <dixoncodes@gmail.com>
2019-12-24 20:46:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,13 @@
 # Your Code Here
 def map(source_array)
-  new_array
+  new_array = []
+  i = 0
   while i < source_array.length
-  new_array.push(source_array[i] )
+  new_array.push(source_array[i])
   i += 1
+  end
+  yield(source_array)
 end
-  yield()
+
+map
   
