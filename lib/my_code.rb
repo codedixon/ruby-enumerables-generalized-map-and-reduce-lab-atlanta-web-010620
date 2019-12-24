6430ea20787b4e136db3f663affc092e4d60fcc0
Rashad Dixon <dixoncodes@gmail.com>
2019-12-24 20:51:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(source_array)
   new_array = []
   i = 0
   while i < source_array.length
-  new_array.push(source_array[i])
+  new_array.push(source_array[i] * )
   i += 1
   end
   yield(new_array)
